f093b9c5a87889094cda5e3fdce113f709f8019c
Matheusbm <matheusmbm13@gmail.com>
2023-11-04 20:30:53 -0300

    add rfid to main cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,15 @@
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN         22           // Configurable, see typical pin layout above
+#define SS_PIN          21          // Configurable, see typical pin layout above
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+
+MFRC522::MIFARE_Key key;
  
 #define AWS_IOT_PUBLISH_TOPIC   "ESP32_01/pub"
 #define AWS_IOT_SUBSCRIBE_TOPIC "ESP32_01/sub"
@@ -74,15 +83,52 @@ void connectAWS()
   Serial.println("AWS IoT Connected!");
 }
   
-void setup()
-{
+
+void setup() {
   Serial.begin(9600);
-  connectAWS();
+  connectAWS();                                           // Initialize serial communications with the PC
+  SPI.begin();                                                  // Init SPI bus
+  mfrc522.PCD_Init();                                              // Init MFRC522 card
+  Serial.println(F("Read personal data on a MIFARE PICC:"));    //shows in serial that it is ready to read
 }
- 
-void loop()
-{
+
+//*****************************************************************************************//
+void loop() {
+
+  // Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
+  MFRC522::MIFARE_Key key;
+  for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
+
+  //some variables we need
+  byte block;
+  byte len;
+  MFRC522::StatusCode status;
+
+  //-------------------------------------------
+
+  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  Serial.println(F("**Card Detected:**"));
+
+  //-------------------------------------------
+
+  mfrc522.PICC_DumpDetailsToSerial(&(mfrc522.uid)); //dump some details about the card
+
   publishMessage();
-  client.loop();
-  delay(1000);
+  
+  Serial.println(F("\n**End Reading**\n"));
+
+  delay(1000); //change value if you want to read cards faster
+
+  mfrc522.PICC_HaltA();
+  mfrc522.PCD_StopCrypto1();
 }
+//*****************************************************************************************//
